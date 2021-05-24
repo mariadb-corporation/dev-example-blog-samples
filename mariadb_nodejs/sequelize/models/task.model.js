080b1e2964ecacf02b34bd8927ebce835a5a99e1
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2021-05-24 10:35:25 -0500

    Connector/Node.js+Sequelize (Task) model updates

--- mariadb_nodejs/sequelize/models/task.model.js
+++ mariadb_nodejs/sequelize/models/task.model.js
@@ -2,13 +2,17 @@ module.exports = (sequelize, Sequelize) => {
     const Task = sequelize.define("task", {
       id: {
         type: Sequelize.INTEGER,
+        autoIncrement: true,
         primaryKey: true
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false 
       }
     });
     return Task;
