f19088c943e9a746ed2da7c5d785d9d9ccaa4d60
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2021-06-01 15:43:08 -0500

    Update server.js

--- mariadb_nodejs/basic/server.js
+++ mariadb_nodejs/basic/server.js
@@ -9,56 +9,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // GET
 app.get('/tasks', async (req, res) => {
-    let conn;
     try {
         const result = await db.pool.query("select * from tasks");
         res.send(result);
     } catch (err) {
         throw err;
-    } finally {
-        if (conn) return conn.release();
     }
 });
 
 // POST
 app.post('/tasks', async (req, res) => {
     let task = req.body;
-    console.log(task);
-    let conn;
     try {
         const result = await db.pool.query("insert into tasks (description) values (?)", [task.description]);
         res.send(result);
     } catch (err) {
         throw err;
-    } finally {
-        if (conn) return conn.release();
     }
 });
 
 app.put('/tasks', async (req, res) => {
     let task = req.body;
-    let conn;
     try {
         const result = await db.pool.query("update tasks set description = ?, completed = ? where id = ?", [task.description, task.completed, task.id]);
         res.send(result);
     } catch (err) {
         throw err;
-    } finally {
-        if (conn) return conn.release();
-    }
+    } 
 });
 
 app.delete('/tasks', async (req, res) => {
     let id = req.query.id;
-    let conn;
     try {
         const result = await db.pool.query("delete from tasks where id = ?", [id]);
         res.send(result);
     } catch (err) {
         throw err;
-    } finally {
-        if (conn) return conn.release();
-    }
+    } 
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
