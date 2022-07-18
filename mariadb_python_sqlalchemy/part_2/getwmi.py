face7c6396de9f7d56c274c100d767a28c64b6b8
awinterl <awinterl@hotmail.de>
2022-07-19 00:27:01 +0200

    Weitere WMI Abfragen

--- mariadb_python_sqlalchemy/part_2/getwmi.py
+++ mariadb_python_sqlalchemy/part_2/getwmi.py
@@ -5,6 +5,10 @@ wmidata = wmi.WMI()
 for os in wmidata.Win32_OperatingSystem():
     print(os.caption, "Build:", os.buildnumber)
 
+p: list = wmidata.Win32_NetworkAdapter()
 
-for d in wmidata.Win32_Product():
-    print(d)
+
+for card in p:
+
+    if card.NetEnabled:
+        print(card.caption, '|', card.NetEnabled, '|', card.MacAddress)
