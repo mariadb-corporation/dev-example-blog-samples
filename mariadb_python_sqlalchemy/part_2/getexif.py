28b07eddd583d6a1f4e6a32418d4e8db9d12e4fa
awinterl <awinterl@hotmail.de>
2022-07-22 13:36:43 +0200

    Abfragen von Bilder Tags

---
+++ mariadb_python_sqlalchemy/part_2/getexif.py
@@ -0,0 +1,18 @@
+from exiftool import ExifToolHelper
+import os
+
+path = "c:\\TestImage"
+
+for file in os.listdir(path):
+    if os.path.isdir(path + file):
+        paths.append(path + file)
+    else:
+        sfile = path + '\\' + file
+        print(sfile)
+        with ExifToolHelper() as et:
+            try:
+                for d in et.get_tags(sfile, tags=['XMP:*', 'IPTC:*', 'EXIF:*']):
+                    for k, v in d.items():
+                        print(f"Dict: {k} = {v}")
+            except:
+                print('Fehler')
