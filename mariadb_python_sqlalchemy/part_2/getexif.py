e7a0c61433aae91f071f8e2774aeb0b8d48b9b9e
awinterl <awinterl@hotmail.de>
2022-07-23 00:33:21 +0200

    Erste Versuche mit der Tabellen Klasse

--- mariadb_python_sqlalchemy/part_2/getexif.py
+++ mariadb_python_sqlalchemy/part_2/getexif.py
@@ -18,6 +18,8 @@ paths = []
 
 session.commit()
 
+fdata = FileData()
+
 for file in os.listdir(path):
     if os.path.isdir(path + '\\' + file):
         paths.append(path + file)
@@ -28,8 +30,13 @@ for file in os.listdir(path):
             try:
                 for d in et.get_tags(sfile, tags=['SourceFile', 'File:Directory', 'File:FileName']):
                     for k, v in d.items():
+                        if k == 'File:FileName':
+                            fdata.filename = v
+                        elif k == 'File:Directory':
+                            fdata.path = v
+                        elif k == 'SourceFile':
+                            fdata.fullname = v
 
-                        print(f"Dict: {k} = {v}")
             except:
                 print('Fehler')
 
